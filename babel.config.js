d75befdf152ba36fb6f58349bb61669340d8c56e
bandit.studios <banditstudiosdpd@gmail.com>
2025-12-20 16:22:19 -0500

    VENT-20 Update configuration files and dependencies
    - Refactored babel.config.js for improved readability and consistency.
    - Updated metro.config.js to use the correct import path for expo's metro-config.
    - Bumped version of nativewind to 4.2.1 and tailwindcss to 4.1.18 in package.json and package-lock.json.
    - Simplified tailwind.config.js by removing unnecessary properties and enhancing theme configuration.
    - Adjusted tsconfig.json to include babel.config.js and updated paths for better module resolution.
    
    These changes enhance the project's configuration, improve dependency management, and streamline the styling setup.

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,9 @@
-module.exports = function(api) {
+module.exports = function (api) {
     api.cache(true);
-
     return {
-        presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
-        }], "nativewind/babel"],
-
-        plugins: [["module-resolver", {
-            root: ["./"],
-
-            alias: {
-                "@": "./",
-                "tailwind.config": "./tailwind.config.js"
-            }
-        }]]
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
     };
-};
+  };
